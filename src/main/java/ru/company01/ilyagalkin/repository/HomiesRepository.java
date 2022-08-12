a8a3e818d388c9944e33c23585f1b510e3b1bbc5
ilya.en.ru.fr.galkin <ilya.en.ru.fr.galkin@gmail.com>
2022-08-12 15:21:45 +0700

    remove excess of comments

--- src/main/java/ru/company01/ilyagalkin/repository/HomiesRepository.java
+++ src/main/java/ru/company01/ilyagalkin/repository/HomiesRepository.java
@@ -12,18 +12,6 @@ import java.util.Optional;
 public interface HomiesRepository extends JpaRepository<Homies, Integer> {
     List<Homies> findByNameContaining(String name);
 
-//    @Query("select u from User u where u.name like concat('%', ?1, '%')")
-//    List<User> findByNameContaining(String name);
-
     Optional<Homies> findByNameLikeIgnoreCase(String name);
-
-
-
-//    @Query("select u from User u where upper(u.name) like upper(:name)")
-//    Optional<Homies> findByNameLikeIgnoreCase_same(@Param("name") String name);
-
-    //same but extracted via JPA Inspector panel
-//    @Query("select u from User u where upper(u.name) like upper(?1)")
-//    Optional<User> findByNameLikeIgnoreCase_anyNameCanBeGiven(String name);
 
 }
